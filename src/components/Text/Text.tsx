61b28dd3f8fbc05e92cc40389a81a005d404e405
Julio Macedo <juliocesar1076@gmail.com>
2025-02-06 10:37:03 -0300

    feat: add font family config

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -8,19 +8,55 @@ import {
 type TextProps = RNTextProps & {
   style?: TextStyle;
   preset?: TextVariants;
+  bold?: boolean;
+  italic?: boolean;
+  semiBold?: boolean;
 };
 
 export function Text({
   preset = 'paragraphMedium',
+  bold,
+  italic,
+  semiBold,
   style,
   children,
   ...restProps
 }: TextProps) {
+  const fontFamily = getFontFamily(preset, bold, italic, semiBold);
   return (
-    <RNText style={[$fontSizes[preset], style]} {...restProps}>
+    <RNText style={[$fontSizes[preset], style, {fontFamily}]} {...restProps}>
       {children}
     </RNText>
   );
+}
+
+function getFontFamily(
+  preset: TextVariants,
+  bold?: boolean,
+  italic?: boolean,
+  semiBold?: boolean,
+) {
+  if (
+    preset === 'headingLarge' ||
+    preset === 'headingSmall' ||
+    preset === 'paragraphMedium'
+  ) {
+    return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+  }
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case italic:
+      return $fontFamily.italic;
+    case semiBold && italic:
+      return $fontFamily.mediumItalic;
+    case semiBold:
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
 }
 
 type TextVariants =
@@ -42,4 +78,23 @@ const $fontSizes: Record<TextVariants, TextStyle> = {
   paragraphSmall: {fontSize: 14, lineHeight: 19.6},
   paragraphCaption: {fontSize: 12, lineHeight: 16.8},
   paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+const $fontFamily = {
+  black: 'Nunito-Black',
+  blackItalic: 'Nunito-BlackItalic',
+  bold: 'Nunito-Bold',
+  boldItalic: 'Nunito-BoldItalic',
+  extraBold: 'Nunito-ExtraBold',
+  extraBoldItalic: 'Nunito-ExtraBoldItalic',
+  extraLight: 'Nunito-ExtraLight',
+  extraLightItalic: 'Nunito-ExtraLightItalic',
+  italic: 'Nunito-Italic',
+  light: 'Nunito-Light',
+  lightItalic: 'Nunito-LightItalic',
+  medium: 'Nunito-Medium',
+  mediumItalic: 'Nunito-MediumItalic',
+  regular: 'Nunito-Regular',
+  semiBold: 'Nunito-SemiBold',
+  semiBoldItalic: 'Nunito-SemiBoldItalic',
 };
