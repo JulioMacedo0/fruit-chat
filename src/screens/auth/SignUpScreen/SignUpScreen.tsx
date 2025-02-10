0983ea64298a7407a891116bf7c93a5cee6a8e91
Julio Macedo <juliocesar1076@gmail.com>
2025-02-10 17:50:46 -0300

    feat: create signup form validation

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
-import {TextInput} from '../../../components/TextInput/TextInput';
 import {Button} from '../../../components/Button/Button';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../routes/Routes';
+import {signUpScreenSchema, SignUpScreenSchema} from './SignUpScreenSchema';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {FormTextInput} from '../../../components/FormTextInput/FormTextInput';
 
 type SignUpScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -12,28 +15,52 @@ type SignUpScreenProps = NativeStackScreenProps<
 >;
 
 export function SignUpScreen({}: SignUpScreenProps) {
+  const {control, formState, handleSubmit} = useForm<SignUpScreenSchema>({
+    resolver: zodResolver(signUpScreenSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  function submitForm(formValues: SignUpScreenSchema) {
+    console.log(formValues);
+  }
+
   return (
     <Screen canGoBack scrollabe>
-      <Text mb="s10" preset="headingLarge">
-        Register
+      <Text mb="s10" preset="headingLarge" color="primary">
+        Registrar
       </Text>
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="username"
         label="Digite seu usarname"
         placeholder="@"
         boxProps={{mb: 's10'}}
       />
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="email"
         label="E-mail"
         placeholder="Digite seu E-mail"
         boxProps={{mb: 's10'}}
       />
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="password"
         label="Senha"
         placeholder="Digite sua senha"
-        boxProps={{mb: 's10'}}
+        boxProps={{mb: 's20'}}
       />
 
-      <Button buttonVariant="fill" title="Criar conta" />
+      <Button
+        buttonVariant="fill"
+        title="Criar conta"
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+      />
     </Screen>
   );
 }
