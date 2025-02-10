0ebaec27bc9326d243dab135735a7a21c0da8d9b
Julio Macedo <juliocesar1076@gmail.com>
2025-02-10 17:28:14 -0300

    feat: create validation in form

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -6,6 +6,11 @@ import {TextInput} from '../../../components/TextInput/TextInput';
 import {Screen} from '../../../components/Screen/Screen';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../routes/Routes';
+import {useForm} from 'react-hook-form';
+import {loginScreenSchema, LoginScreenSchema} from './LoginScreenSchema';
+
+import {zodResolver} from '@hookform/resolvers/zod';
+import {FormTextInput} from '../../../components/FormTextInput/FormTextInput';
 
 type LoginScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -13,28 +18,56 @@ type LoginScreenProps = NativeStackScreenProps<
 >;
 
 export function LoginScreen({navigation}: LoginScreenProps) {
+  const {control, formState, handleSubmit} = useForm<LoginScreenSchema>({
+    resolver: zodResolver(loginScreenSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function submitForm(formValues: LoginScreenSchema) {
+    console.log(formValues);
+  }
   return (
     <Screen scrollabe>
       <Text mb="s10" preset="headingLarge" color="primary">
         Login
       </Text>
 
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="email"
         keyboardType="email-address"
         label="E-mail"
         placeholder="Digite seu E-mail"
         returnKeyType="next"
+        boxProps={{
+          mb: 's12',
+        }}
       />
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="password"
         label="Senha"
         placeholder="Digite sua senha"
         returnKeyType="done"
+        boxProps={{
+          mb: 's12',
+        }}
       />
 
-      <Text mt="s10" preset="paragraphSmall" color="primary" bold>
+      <Text preset="paragraphSmall" color="primary" bold>
         Esqueceu a senha?
       </Text>
-      <Button mt="s56" buttonVariant="fill" title="Entrar" mb="s10" />
+      <Button
+        mt="s56"
+        buttonVariant="fill"
+        title="Entrar"
+        mb="s10"
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+      />
       <Button
         buttonVariant="outline"
         title="Criar conta"
