f4860d097850a600700e70f1819b0f386a41693c
Julio Macedo <juliocesar1076@gmail.com>
2025-02-11 10:22:53 -0300

    feat: setup absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,30 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.ts',
+          '.android.tsx',
+          '.ts',
+          '.tsx',
+          '.js',
+          '.jsx',
+          '.json',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@theme': './src/theme',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
