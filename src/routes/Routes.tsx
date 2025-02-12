557144aa5aa6a542ba153f5a6a7897e97f91e253
Julio Macedo <juliocesar1076@gmail.com>
2025-02-12 10:34:44 -0300

    feat: add auth flow

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {LoginScreen} from '../screens/auth/LoginScreen/LoginScreen';
-import {SignUpScreen} from '../screens/auth/SignUpScreen/SignUpScreen';
+import {AppStack} from './AppStack';
+import {AuthStack} from './AuthStack';
 
-export type RootStackParamList = {
-  LoginScreen: undefined;
-  SignUpScreen: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const authenticated = true;
 
 export function Router() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoginScreen"
-        screenOptions={{
-          headerShown: false,
-          fullScreenGestureEnabled: true,
-        }}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-      </Stack.Navigator>
+      {authenticated ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
 }
