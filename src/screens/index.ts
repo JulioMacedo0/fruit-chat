e94e2116dcb41470f3f7c2132305d4d5fbb814fb
Julio Macedo <juliocesar1076@gmail.com>
2025-02-12 10:34:50 -0300

    feat: export type

---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+export * from './auth/LoginScreen/LoginScreen';
+export * from './auth/LoginScreen/LoginScreenSchema';
+export * from './auth/SignUpScreen/SignUpScreen';
+export * from './auth/SignUpScreen/SignUpScreenSchema';
+
+export * from './app/HomeScreen/HomeScreen';
