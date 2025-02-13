09007f85beb25713d30744d5302b27da6bbf21d8
Julio Macedo <juliocesar1076@gmail.com>
2025-02-13 17:39:56 -0300

    feat: add separator

---
+++ src/components/ItemSeparator/ItemSeparator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Box} from '@components';
+
+export function ItemSeparator() {
+  return (
+    <Box height={1} bg="primary" marginHorizontal="s10" marginVertical="s10" />
+  );
+}
