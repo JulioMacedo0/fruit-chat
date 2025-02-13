e6770e36c3073cee277bbb325314ab15b8b9ea1e
Julio Macedo <juliocesar1076@gmail.com>
2025-02-13 16:13:16 -0300

    feat: create user mock

---
+++ src/constants/user.mock.ts
@@ -0,0 +1,7 @@
+import {Participant} from 'Types';
+
+export const user: Participant = {
+  id: '+5563999531433',
+  name: 'Julio Macedo',
+  profilePicture: 'https://github.com/JulioMacedo0.png',
+};
