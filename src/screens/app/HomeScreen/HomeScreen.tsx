c73c8891b9053075449c3eba2346b4aa9103ade3
Julio Macedo <juliocesar1076@gmail.com>
2025-02-13 12:01:21 -0300

    feat: use chat item

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
+import {FlatList} from 'react-native';
 
 import {HeaderApp} from 'components/HeaderApp/HeaderApp';
+import {Chat} from 'Types';
 
-import {Screen, Text} from '@components';
+import {Box, ChatItem, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
+
+const chats: Chat[] = [
+  {
+    chat_id: 'group-12345',
+    type: 'private',
+    name: 'Maria Oliveira',
+    participants: [
+      {id: '+5511999999999', name: 'João Silva'},
+      {id: '+5511988888888', name: 'Maria Oliveira'},
+    ],
+    messages: [
+      {
+        message_id: 'msg-001',
+        timestamp: '2025-02-13T12:34:56Z',
+        from: '+5511999999999',
+        to: '+5511988888888',
+        content: {
+          type: 'text',
+          text: 'Olá, tudo bem?',
+        },
+        status: {
+          received: true,
+          read: false,
+        },
+      },
+      {
+        message_id: 'msg-002',
+        timestamp: '2025-02-13T12:35:10Z',
+        from: '+5511988888888',
+        to: '+5511999999999',
+        content: {
+          type: 'text',
+          text: 'Oi! Tudo ótimo e você?',
+        },
+        status: {
+          received: true,
+          read: true,
+        },
+      },
+    ],
+  },
+];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
   return (
-    <Screen scrollabe>
+    <Screen>
       <HeaderApp />
-      <Text>Home</Text>
+      <FlatList renderItem={item => <ChatItem {...item.item} />} data={chats} />
     </Screen>
   );
 }
