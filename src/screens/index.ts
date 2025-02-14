3cab37dd6e4f65e545a580d03b172316c5c6eb40
Julio Macedo <juliocesar1076@gmail.com>
2025-02-14 09:43:53 -0300

    feat: export new page

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,8 +1,13 @@
+//Auth Stack
 export * from './auth/LoginScreen/LoginScreen';
 export * from './auth/LoginScreen/LoginScreenSchema';
 export * from './auth/SignUpScreen/SignUpScreen';
 export * from './auth/SignUpScreen/SignUpScreenSchema';
 
+//App Stack
+export * from './app/ChatScreen/ChatScreen';
+
+//AppTabNavigator Stack
 export * from './app/HomeScreen/HomeScreen';
 export * from './app/CallsScreen/CallsScreen';
 export * from './app/StatusScreen/StatusScreen';
