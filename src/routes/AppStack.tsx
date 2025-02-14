d8cfd95cd7dd490401cb33bbda1faf4e8da20a5f
Julio Macedo <juliocesar1076@gmail.com>
2025-02-14 09:43:48 -0300

    feat: add new page

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import {NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Participant} from '@types';
+
+import {ChatScreen} from '@screens';
 
 import {AppTabBottomTabParamList, AppTabNavigator} from './AppTabNavigator';
 
 export type AppStackParamList = {
   AppTabNavigator: NavigatorScreenParams<AppTabBottomTabParamList>;
+  ChatScreen: {participant: Pick<Participant, 'id'>};
 };
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
@@ -20,6 +24,7 @@ export function AppStack() {
         fullScreenGestureEnabled: true,
       }}>
       <Stack.Screen name="AppTabNavigator" component={AppTabNavigator} />
+      <Stack.Screen name="ChatScreen" component={ChatScreen} />
     </Stack.Navigator>
   );
 }
